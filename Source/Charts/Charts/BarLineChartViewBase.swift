f97e3ec5978cb95b74c7fa9506eed4c66f4ba697
Greg Hart <gregory.james.hart@gmail.com>
2017-11-14 17:17:11 -0600

    Updates the max visible count to 1000

--- Source/Charts/Charts/BarLineChartViewBase.swift
+++ Source/Charts/Charts/BarLineChartViewBase.swift
@@ -26,7 +26,7 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
 {
     /// the maximum number of entries to which values will be drawn
     /// (entry numbers greater than this value will cause value-labels to disappear)
-    @objc internal var _maxVisibleCount = 100
+    @objc internal var _maxVisibleCount = 1000
     
     /// flag that indicates if auto scaling on the y axis is enabled
     fileprivate var _autoScaleMinMaxEnabled = false
